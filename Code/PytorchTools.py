487aa4a5d794176cf069777f799ea6bff68aabb3
zpliulab <54654413+zpliulab@users.noreply.github.com>
2022-07-27 12:15:45 +0800

    updated

--- Code/PytorchTools.py
+++ Code/PytorchTools.py
@@ -7,13 +7,13 @@ class EarlyStopping:
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
-                            上次验证集损失值改善后等待几个epoch
+                           
                             Default: 7
             verbose (bool): If True, prints a message for each validation loss improvement.
-                            如果是True，为每个验证集损失值改善打印一条信息
+                            
                             Default: False
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            监测数量的最小变化，以符合改进的要求
+                           
                             Default: 0
         """
         self.patience = patience
@@ -45,11 +45,11 @@ class EarlyStopping:
     def save_checkpoint(self, val_loss, model):
         '''
         Saves model when validation loss decrease.
-        验证损失减少时保存模型。
+       
         '''
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        # torch.save(model.state_dict(), 'checkpoint.pt')     # 这里会存储迄今最优模型的参数
-        torch.save(model.state_dict(), self.save_dir+'.pkl')                 # 这里会存储迄今最优的模型
+        # torch.save(model.state_dict(), 'checkpoint.pt')     
+        torch.save(model.state_dict(), self.save_dir+'.pkl')                 
         self.val_loss_min = val_loss
 
